70c971d74782e362abca468a98390ac16b7d7f3b
Chandler Tu <chandlertu@chandlertu.com>
2019-04-15 23:23:09 +0800

    增加登陆页面

--- vue-web-demo/src/App.vue
+++ vue-web-demo/src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Login />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Login from "./components/Login.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Login
   }
-}
+};
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  background: url(./assets/background.png) center;
+  background-size: cover;
 }
 </style>
---
+++ vue-web-demo/src/components/Login.vue
@@ -0,0 +1,86 @@
+<template>
+  <el-card header="登陆">
+    <el-form
+      :model="form"
+      ref="form"
+      status-icon
+      :rules="rules"
+    >
+      <el-form-item prop="username">
+        <el-input
+          v-model="form.username"
+          placeholder="用户名"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          v-model="form.password"
+          type="password"
+          placeholder="密码"
+        ></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button
+          type="primary"
+          @click="submitForm('form')"
+          style="width: 100%;"
+        >登陆</el-button>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  data() {
+    var checkUsername = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("请输入用户名"));
+      } else {
+        callback();
+      }
+    };
+    var checkPassword = (rule, value, callback) => {
+      if (!value) {
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      form: {
+        username: "",
+        password: ""
+      },
+      rules: {
+        username: [{ validator: checkUsername, trigger: "blur" }],
+        password: [{ validator: checkPassword, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+.el-card {
+  position: fixed;
+  left: 38%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  opacity: 0.62;
+}
+.el-card:hover {
+  opacity: 1;
+}
+</style>
