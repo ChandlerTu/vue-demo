ac096d2da207d4aa5234b27a6cf21dd46bf13d36
Chandler Tu <chandlertu@chandlertu.com>
2019-04-15 20:43:38 +0800

    安装 vue-router、element-ui
    cd vue-web-demo
    npm i vue-router
    npm i element-ui -S

--- vue-web-demo/src/main.js
+++ vue-web-demo/src/main.js
@@ -1,4 +1,12 @@
 import Vue from 'vue'
+
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
