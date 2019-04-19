b3b5e531b33fc9f88a7e4bf8489682b37e3a2be1
tuchangle <tuchangle@163.com>
2019-04-19 11:17:18 +0800

    增加 Mixin 基本用法

---
+++ vue-mixin-demo/src/components/mixin.js
@@ -0,0 +1,19 @@
+import hello from "./hello.js";
+
+export default {
+    data: function () {
+        return {
+            message: 'hello',
+            foo: 'abc'
+        }
+    },
+    created: function () {
+        this.hello()
+    },
+    methods: {
+        hello: function () {
+            console.log('hello from mixin!')
+            hello.hello();
+        }
+    }
+}
